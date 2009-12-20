25e4267a1d803b377e6bbc2d935b9eb10903c7b7
Dan Stahlke <dstahlke@gmail.com>
2009-12-19 18:48:46 -0900

    pimpl for plot canvas

--- scopemm-plotcanvas.cc
+++ scopemm-plotcanvas.cc
@@ -4,174 +4,36 @@
 #include "scopemm.h"
 #include "scopemm-layerimpl.h"
 
-using namespace scopemm;
+namespace scopemm {
 
-/// PlotCanvas ////////////////////////////////////////
+/// PlotCanvasImpl ////////////////////////////////////
 
-PlotCanvas::PlotCanvas() :
+class PlotCanvasImpl {
+public:
+	PlotCanvasImpl();
+
+	void recalcAutoRange();
+	void recalcAffine();
+
+	std::set<PlotLayerImplPtr> layers;
+	// pointer is needed here because GridLayer is an incomplete type
+	std::auto_ptr<GridLayer> grid_layer;
+	bool x_auto, y_auto;
+	bool draw_x_axis, draw_y_axis;
+	int screen_w, screen_h;
+	Bbox bbox;
+	AffineTransform affine;
+	bool swap_axes;
+};
+
+PlotCanvasImpl::PlotCanvasImpl() : 
 	grid_layer(new GridLayer()),
 	x_auto(true), y_auto(true),
 	draw_x_axis(false), draw_y_axis(false),
 	bbox(0, 1, 0, 1), swap_axes(false)
 { }
 
-PlotCanvas::~PlotCanvas() {
-	// now that our object no longer exists, it must not receive
-	// any more events
-	BOOST_FOREACH(const PlotLayerImplPtr &layer, layers) {
-		layer->change_listeners.erase(this);
-	}
-}
-
-PlotCanvas &PlotCanvas::addLayer(PlotLayerBase &layer) {
-	layer.impl_base->change_listeners.insert(this);
-	layers.insert(layer.impl_base);
-	fireChangeEvent();
-	return *this;
-}
-
-PlotCanvas &PlotCanvas::removeLayer(PlotLayerBase &layer) {
-	layer.impl_base->change_listeners.erase(this);
-	layers.erase(layer.impl_base);
-	fireChangeEvent();
-	return *this;
-}
-
-void PlotCanvas::setXAutoRange() {
-	x_auto = true;
-	recalcAutoRange();
-	fireChangeEvent();
-}
-
-void PlotCanvas::setYAutoRange() {
-	y_auto = true;
-	recalcAutoRange();
-	fireChangeEvent();
-}
-
-void PlotCanvas::setXRange(double min, double max) {
-	bbox.xmin = min;
-	bbox.xmax = max;
-	x_auto = false;
-	recalcAffine();
-	fireChangeEvent();
-}
-
-void PlotCanvas::setYRange(double min, double max) {
-	bbox.ymin = min;
-	bbox.ymax = max;
-	y_auto = false;
-	recalcAffine();
-	fireChangeEvent();
-}
-
-void PlotCanvas::setBbox(Bbox new_bbox) {
-	bbox = new_bbox;
-	x_auto = false;
-	y_auto = false;
-	recalcAffine();
-	fireChangeEvent();
-}
-
-void PlotCanvas::setSwapAxes(bool state) {
-	swap_axes = state;
-	fireChangeEvent();
-}
-
-void PlotCanvas::setDrawAxes(bool state) {
-	draw_x_axis = state;
-	draw_y_axis = state;
-	fireChangeEvent();
-}
-
-void PlotCanvas::setDrawXAxis(bool state) {
-	draw_x_axis = state;
-	fireChangeEvent();
-}
-
-void PlotCanvas::setDrawYAxis(bool state) {
-	draw_y_axis = state;
-	fireChangeEvent();
-}
-
-void PlotCanvas::setDrawGrids(bool state) {
-	setDrawGrids(state, state);
-}
-
-void PlotCanvas::setDrawGrids(bool xgrid, bool ygrid) {
-	if(xgrid || ygrid) {
-		grid_layer->setDrawXGrid(xgrid);
-		grid_layer->setDrawYGrid(ygrid);
-		addLayer(*grid_layer);
-	} else {
-		removeLayer(*grid_layer);
-	}
-	fireChangeEvent();
-}
-
-bool PlotCanvas::on_expose_event(GdkEventExpose* event) {
-	Glib::RefPtr<Gdk::Window> window = get_window();
-
-	if(!window) return true;
-
-	screen_w = get_allocation().get_width();
-	screen_h = get_allocation().get_height();
-	recalcAffine();
-
-	Cairo::RefPtr<Cairo::Context> cr = window->create_cairo_context();
-	if(event) {
-		cr->rectangle(event->area.x, event->area.y,
-			event->area.width, event->area.height);
-		cr->clip();
-	}
-
-	cr->save();
-	cr->set_source_rgb(1, 1, 1);
-	cr->paint();
-	cr->restore();
-
-	if(draw_x_axis || draw_y_axis) {
-		cr->save();
-		cr->set_line_width(1);
-		cr->set_source_rgb(0.3, 0.3, 0.3);
-		cr->set_antialias(Cairo::ANTIALIAS_NONE);
-		if(draw_x_axis) {
-			double x1, y1, x2, y2;
-			coordToScreen(0, bbox.ymin, x1, y1);
-			coordToScreen(0, bbox.ymax, x2, y2);
-			cr->move_to(x1, y1);
-			cr->line_to(x2, y2);
-			cr->stroke();
-		}
-		if(draw_y_axis) {
-			double x1, y1, x2, y2;
-			coordToScreen(bbox.xmin, 0, x1, y1);
-			coordToScreen(bbox.xmax, 0, x2, y2);
-			cr->move_to(x1, y1);
-			cr->line_to(x2, y2);
-			cr->stroke();
-		}
-		cr->restore();
-	}
-
-	typedef std::pair<double, PlotLayerImplPtr> zbuf_element;
-	std::set<zbuf_element> zsorted;
-	BOOST_FOREACH(const PlotLayerImplPtr &layer, layers) {
-		zsorted.insert(zbuf_element(layer->zorder, layer));
-	}
-	BOOST_FOREACH(const zbuf_element &layer, zsorted) {
-		layer.second->draw(this, cr);
-	}
-
-	return true;
-}
-
-void PlotCanvas::fireChangeEvent() {
-	recalcAutoRange();
-	queue_draw();
-}
-
-void PlotCanvas::recalcAutoRange() {
+void PlotCanvasImpl::recalcAutoRange() {
 	if(x_auto) {
 		bool first = true;
 		double min=0, max=0;
@@ -212,9 +74,177 @@ void PlotCanvas::recalcAutoRange() {
 	}
 }
 
-void PlotCanvas::recalcAffine() {
+void PlotCanvasImpl::recalcAffine() {
 	Bbox screen_bbox(0, screen_w, screen_h, 0);
 	affine = AffineTransform::boxToBox(bbox, screen_bbox, swap_axes);
+}
+
+/// PlotCanvas ////////////////////////////////////////
+
+PlotCanvas::PlotCanvas() :
+	impl(new PlotCanvasImpl())
+{ }
+
+PlotCanvas::~PlotCanvas() {
+	// now that our object no longer exists, it must not receive
+	// any more events
+	BOOST_FOREACH(const PlotLayerImplPtr &layer, impl->layers) {
+		layer->change_listeners.erase(this);
+	}
+}
+
+PlotCanvas &PlotCanvas::addLayer(PlotLayerBase &layer) {
+	layer.impl_base->change_listeners.insert(this);
+	impl->layers.insert(layer.impl_base);
+	fireChangeEvent();
+	return *this;
+}
+
+PlotCanvas &PlotCanvas::removeLayer(PlotLayerBase &layer) {
+	layer.impl_base->change_listeners.erase(this);
+	impl->layers.erase(layer.impl_base);
+	fireChangeEvent();
+	return *this;
+}
+
+void PlotCanvas::setXAutoRange() {
+	impl->x_auto = true;
+	impl->recalcAutoRange();
+	fireChangeEvent();
+}
+
+void PlotCanvas::setYAutoRange() {
+	impl->y_auto = true;
+	impl->recalcAutoRange();
+	fireChangeEvent();
+}
+
+void PlotCanvas::setXRange(double min, double max) {
+	impl->bbox.xmin = min;
+	impl->bbox.xmax = max;
+	impl->x_auto = false;
+	impl->recalcAffine();
+	fireChangeEvent();
+}
+
+void PlotCanvas::setYRange(double min, double max) {
+	impl->bbox.ymin = min;
+	impl->bbox.ymax = max;
+	impl->y_auto = false;
+	impl->recalcAffine();
+	fireChangeEvent();
+}
+
+void PlotCanvas::setBbox(Bbox new_bbox) {
+	impl->bbox = new_bbox;
+	impl->x_auto = false;
+	impl->y_auto = false;
+	impl->recalcAffine();
+	fireChangeEvent();
+}
+
+void PlotCanvas::setSwapAxes(bool state) {
+	impl->swap_axes = state;
+	fireChangeEvent();
+}
+
+void PlotCanvas::setDrawAxes(bool state) {
+	impl->draw_x_axis = state;
+	impl->draw_y_axis = state;
+	fireChangeEvent();
+}
+
+void PlotCanvas::setDrawXAxis(bool state) {
+	impl->draw_x_axis = state;
+	fireChangeEvent();
+}
+
+void PlotCanvas::setDrawYAxis(bool state) {
+	impl->draw_y_axis = state;
+	fireChangeEvent();
+}
+
+void PlotCanvas::setDrawGrids(bool state) {
+	setDrawGrids(state, state);
+}
+
+void PlotCanvas::setDrawGrids(bool xgrid, bool ygrid) {
+	if(xgrid || ygrid) {
+		impl->grid_layer->setDrawXGrid(xgrid);
+		impl->grid_layer->setDrawYGrid(ygrid);
+		addLayer(*impl->grid_layer);
+	} else {
+		removeLayer(*impl->grid_layer);
+	}
+	fireChangeEvent();
+}
+
+const Bbox &PlotCanvas::getBbox() const { return impl->bbox; }
+
+const AffineTransform &PlotCanvas::getAffine() const { return impl->affine; }
+
+bool PlotCanvas::getSwapAxes() const { return impl->swap_axes; }
+
+bool PlotCanvas::on_expose_event(GdkEventExpose* event) {
+	Glib::RefPtr<Gdk::Window> window = get_window();
+
+	if(!window) return true;
+
+	impl->screen_w = get_allocation().get_width();
+	impl->screen_h = get_allocation().get_height();
+	impl->recalcAffine();
+
+	Cairo::RefPtr<Cairo::Context> cr = window->create_cairo_context();
+	if(event) {
+		cr->rectangle(event->area.x, event->area.y,
+			event->area.width, event->area.height);
+		cr->clip();
+	}
+
+	cr->save();
+	cr->set_source_rgb(1, 1, 1);
+	cr->paint();
+	cr->restore();
+
+	if(impl->draw_x_axis || impl->draw_y_axis) {
+		cr->save();
+		cr->set_line_width(1);
+		cr->set_source_rgb(0.3, 0.3, 0.3);
+		cr->set_antialias(Cairo::ANTIALIAS_NONE);
+		if(impl->draw_x_axis) {
+			double x1, y1, x2, y2;
+			coordToScreen(0, impl->bbox.ymin, x1, y1);
+			coordToScreen(0, impl->bbox.ymax, x2, y2);
+			cr->move_to(x1, y1);
+			cr->line_to(x2, y2);
+			cr->stroke();
+		}
+		if(impl->draw_y_axis) {
+			double x1, y1, x2, y2;
+			coordToScreen(impl->bbox.xmin, 0, x1, y1);
+			coordToScreen(impl->bbox.xmax, 0, x2, y2);
+			cr->move_to(x1, y1);
+			cr->line_to(x2, y2);
+			cr->stroke();
+		}
+		cr->restore();
+	}
+
+	typedef std::pair<double, PlotLayerImplPtr> zbuf_element;
+	std::set<zbuf_element> zsorted;
+	BOOST_FOREACH(const PlotLayerImplPtr &layer, impl->layers) {
+		zsorted.insert(zbuf_element(layer->zorder, layer));
+	}
+	BOOST_FOREACH(const zbuf_element &layer, zsorted) {
+		layer.second->draw(this, cr);
+	}
+
+	return true;
+}
+
+void PlotCanvas::fireChangeEvent() {
+	impl->recalcAutoRange();
+	queue_draw();
 }
 
 /// PlotLayerBase ////////////////////////////////////
@@ -229,3 +259,5 @@ void PlotLayerBase::fireChangeEvent() {
 		l->fireChangeEvent();
 	}
 }
+
+} // namespace scopemm
--- scopemm-plotcanvas.h
+++ scopemm-plotcanvas.h
@@ -15,10 +15,12 @@
 
 namespace scopemm {
 
-class PlotLayerBase;
+class PlotCanvasImpl;
+typedef boost::shared_ptr<PlotCanvasImpl> PlotCanvasImplPtr;
+
 class PlotLayerImplBase;
 typedef boost::shared_ptr<PlotLayerImplBase> PlotLayerImplPtr;
-class GridLayer;
+class PlotLayerBase;
 
 class PlotCanvas : 
 	public Gtk::DrawingArea,
@@ -46,34 +48,23 @@ public:
 	void setDrawXGrid(bool state=true);
 	void setDrawYGrid(bool state=true);
 
-	const Bbox &getBbox() const { return bbox; }
-	const AffineTransform &getAffine() const { return affine; }
-	bool getSwapAxes() const { return swap_axes; }
+	const Bbox &getBbox() const;
+	const AffineTransform &getAffine() const;
+	bool getSwapAxes() const;
 
 	void coordToScreen(double xi, double yi, double &xo, double &yo) const {
-		affine.fwd(xi, yi, xo, yo);
+		getAffine().fwd(xi, yi, xo, yo);
 	}
 
 	void screenToCoord(double xi, double yi, double &xo, double &yo) const {
-		affine.inv(xi, yi, xo, yo);
+		getAffine().inv(xi, yi, xo, yo);
 	}
 
 	bool on_expose_event(GdkEventExpose* event);
 	void fireChangeEvent();
 
 private:
-	void recalcAutoRange();
-	void recalcAffine();
-
-	std::set<PlotLayerImplPtr> layers;
-	// pointer is needed here because GridLayer is an incomplete type
-	std::auto_ptr<GridLayer> grid_layer;
-	bool x_auto, y_auto;
-	bool draw_x_axis, draw_y_axis;
-	int screen_w, screen_h;
-	Bbox bbox;
-	AffineTransform affine;
-	bool swap_axes;
+	PlotCanvasImplPtr impl;
 };
 
 class PlotLayerBase {
